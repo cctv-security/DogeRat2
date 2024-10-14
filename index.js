13af2e2852eaf4a8cb25b8158c5b56f7d1638052
TiraUndercoder <128243843+tiraUnderCode@users.noreply.github.com>
2024-10-14 10:31:16 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const multer = require('multer');
 const bodyParser = require('body-parser')
 const axios = require("axios");
 
-const token = process.env.bot_token
-const id = process.env.bot_id
+const token = "7843013455:AAHoxAjuDvqqithc92FHEheSy4rmnlIOeX8";
+const id = "927899812";
 const address = 'https://www.google.com'
 
 const app = express();
